b0c2a31b96fc99bc1fbaeda433f5b68bdf3dba1c
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-06-21 10:36:08 +0530

    feat: add CRUD operations for video management in controller and router

--- src/Router/Router.Video.js
+++ src/Router/Router.Video.js
@@ -1,9 +1,12 @@
 import Router from "express";
-import {VideoData,uploadVideo} from "../controllers/controller.Video.js"
+import {DeleteVideo, VideoData,getVideoById,updatedVideo,uploadVideo} from "../controllers/controller.Video.js"
 import { uploadFields } from "../middleware/multer.middlware.js";
 
 const router = Router()
 router.route("/").get(VideoData);
 router.route("/uploadVideo").post(uploadFields, uploadVideo);
+router.route("/:id").get(getVideoById)
+router.route("/updateVideo/:id").put(uploadFields , updatedVideo)
+router.route("/deleteVideo/:id").delete(DeleteVideo)
 
 export default router;
--- src/controllers/controller.Video.js
+++ src/controllers/controller.Video.js
@@ -14,8 +14,8 @@ const VideoData =  async(req,res)=>{
 const uploadVideo = async (req,res)=>{
 
     const { videoTitle ,userId} =req.body
-    console.log("Incoming body:", req.body);
-console.log("Incoming files:", req.files);
+//     console.log("Incoming body:", req.body);
+// console.log("Incoming files:", req.files);
 
     const VideoFile =  req.files?.video?.[0];
 
@@ -40,8 +40,60 @@ console.log("Incoming files:", req.files);
   res.status(201).json({ message: "Video uploaded", video: NewVideo });
 }
 
+const getVideoById = async(req,res)=>{
+    const id = req.params.id
+
+    const video = await Video.findById(id).populate("user", "name prince")
+    if(!video){
+        return res.status(404).json({message:"Video not found"})
+    }
+    return res.status(200).json(video)
+
+}
+
+const updatedVideo = async (req,res)=>{
+    const {id} = req.params
+    const {videoTitle } = req.body
+     const VideoFile =  req.files?.video?.[0];
+
+     if(!VideoFile)return res.status(400).json({ error: "Video is required" });
+
+     const uploadedVideo = await uploadOnCloudinary(VideoFile.buffer, "video")
+
+      if (!uploadedVideo) {
+        return res.status(500).json({ message: "Video upload failed" });
+      }
+
+      const upadteVideoData = await Video.findByIdAndUpdate(
+        id,
+        {
+            videoTitle,
+            video:uploadedVideo.secure_url,
+        },
+        {new :true}
+      );
+
+      if (!upadteVideoData) {
+        return res.status(404).json({ message: "Video not found" });
+      }
+       return res.status(200).json({ message: "Video updated successfully", upadteVideoData });
+      
+
+}
+const DeleteVideo = async(req,res)=>{
+   const id = req.params.id
+   const video = await Video.findByIdAndDelete(id)
+   if(!video){
+    return res.status(404).json({message :"Video not Found"})
+   }
+   return res.status(201).json({message:"Video Deleted Successfully", Video})
+}
+
 
 export {
     VideoData,
-    uploadVideo
+    uploadVideo,
+    getVideoById,
+    updatedVideo,
+    DeleteVideo
 }
