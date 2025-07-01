35d9e4f317415deed0dc75ffa64edb66d48c8cdb
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-01 10:41:51 +0530

    feat: import cookie-parser for session management

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './DB/DB.connect.js';
 import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+
 
 
 const app = express();
