ba5a35e92118c8b6afb0c3a81898cbdda8c9a2c4
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-30 19:52:16 +0530

    `Added updateVideo route to aggregationPop.router.js`

--- src/Router/aggragationPop.router.js
+++ src/Router/aggragationPop.router.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { getData,popUploadedVideo,deletePopVideo,GetDataById } from "../controllers/aggregationAndPopulate.js";
+import { getData,popUploadedVideo,deletePopVideo,GetDataById,updateVdieo } from "../controllers/aggregationAndPopulate.js";
 import { uploadedVideo } from "../middleware/multer.middlware.js";
 
 
@@ -9,6 +9,7 @@ router.route("/").get(getData)
 router.route("/upload").post(uploadedVideo, popUploadedVideo) 
 router.route("/delete/:id").delete( deletePopVideo) 
 router.route("/getDataById/:id").get( GetDataById) 
+router.route("/updateVideoById/:id").put(uploadedVideo, updateVdieo) 
 
 
 export default router
