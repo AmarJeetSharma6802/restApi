8799d956c85b54aa8ef782a791adbc936204d832
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-04 19:14:33 +0530

    Add production model with schema definition for realForm

---
+++ src/model/production.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+  },
+  {
+    timeseries: true,
+  }
+);
+
+const realForm = mongoose.model("realForm", productionSchema);
+
+export default realForm;
