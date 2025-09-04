b502c26db652dd2b780c6008560e8bd6ab6ac180
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-04 12:47:04 +0530

    `Added forgotPassword and resetPassword routes to user router`

--- src/Router/user.router.js
+++ src/Router/user.router.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import {uploadImage} from "../middleware/multer.middlware.js"
-import {uploadData,getData,loginUser,deleteAccount,UserloggedOut,updateAccount,userChangePassword,findUserById,refreshToken} from "../controllers/user.controller.js"
+import {uploadData,getData,loginUser,deleteAccount,UserloggedOut,updateAccount,userChangePassword,findUserById,refreshToken,forgotPassword,resetPassword} from "../controllers/user.controller.js"
 import {authUser} from"../middleware/auth.middlware.js"
 
 const router = Router()
@@ -15,6 +15,9 @@ router.route("/loggedOut").post(authUser,UserloggedOut);
 router.route("/updateAccount").put(authUser,uploadImage,updateAccount);
 router.route("/userChangePassword").post(authUser,userChangePassword);
 router.route("/refreshToken").post(refreshToken);
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/resetPassword").post(resetPassword);
+
 
 
 export default router
