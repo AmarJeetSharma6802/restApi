813e3a3ba1c8427f40df5669c1f405ba562de6db
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-20 17:06:11 +0530

    `Update DB.connect.js to connect to MongoDB Express instance`

--- src/DB/DB.connect.js
+++ src/DB/DB.connect.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 const  connectDB = async()=>{
     try {
         // const connect = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_DB_NAME}`)
-        const connect = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        // const connect = await mongoose.connect(`${process.env.MONGODB_URI}`)
+      const connect =  await mongoose.connect(`${process.env.MONGODB_EXPRESS}`);
+
         console.log(`MongoDB connected: ${connect.connection.host}`)
     } catch (error) {
         console.error(error)
